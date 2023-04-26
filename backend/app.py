4417922b25b0536ebcd2e3efafeb1f4df61f6566
Diana-Cano <dianacanoo@icloud.com>
2023-04-25 21:07:40 -0700

    supabase linked, models created

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,49 @@
 from flask import Flask
+from supabase import create_client  
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:itbetterwork!!@db.ickadgsgqqzzjjdoqpdn.supabase.co:5432/postgres'
 
+db.init_app(app)
+
+class User(db.Model):
+    __tablename__= 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    user_name = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(200), nullable = False, unique = True)
+    password = db.Column(db.String(200), nullable = False)
+    workouts = db.relationship('Workout', backref = 'user', lazy = True)
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+    id = db.Column(db.Integer, primary_key = True)
+    ex_date = db.Column(db.Date)
+    exercise = db.Column(db.String(150))
+    reps = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    exercises = db.relationship('Exercise', backref = 'workout', lazy = True)
+   
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+    id = db.Column(db.Integer, primary_key = True)
+    muscle_group = db.Column(db.String(200), nullable = False)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable = False)
+    workout = db.relationship('Workout', backref = 'exercises')
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/testings")
-def testings():
-    return{"testings":["1","2","3"]}
+
+def testing():
+    return {"testings":["1","2","3"]}
+
+if __name__ == "__main__":
+ app.run(debug=True)
 
 
-if __name__ =="__main__":
-    app.run(debug=True)
